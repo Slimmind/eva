26073b43cab04eafc325c78e26b66b9febb9802d
slimmind <slimmind@gmail.com>
2017-02-27 22:48:54 +0200

    added slider

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,10 @@
-/* //= includes/plugin_name.js */
+//= includes/slick.js
 
 (function ($, window, document) {
   'use strict';
   var page = {
     init: function () {
+      page.topSlider();
     },
     noDev: function () {
       if ((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)) {
@@ -12,6 +13,24 @@
     },
     isDev: function () {
       return !$('html').hasClass('no-dev') || window.innerWidth < 1280;
+    },
+    topSlider: function () {
+      var sliderWrap = $('.top-slider');
+      sliderWrap.find('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      sliderWrap.find('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
     },
     load: function () {
     },
